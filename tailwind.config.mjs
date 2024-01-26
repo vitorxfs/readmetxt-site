5e351df39ee6cd56902866ae03406e20c3bd41b1
Vitor Sanches <viisanches98@gmail.com>
2024-01-25 23:32:18 -0300

    feat: add header

---
+++ tailwind.config.mjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Montserrat Variable', ...defaultTheme.fontFamily.sans],
+        mono: ['Red Hat Mono Variable', ...defaultTheme.fontFamily.mono],
+        icon: ['Material Icons']
+      },
+      colors: {
+        'primary': '#C9A3FF',
+        'primary-dark': '#8463B3'
+      }
+    },
+  },
+  plugins: [],
+};
