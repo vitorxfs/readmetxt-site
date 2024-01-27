c9cf95ff66d93b5cec2bcf9fdaae0fc4374a5ee4
Vitor Sanches <viisanches98@gmail.com>
2024-01-27 11:46:35 -0300

    feat: add hero section

---
+++ src/content/hero.ts
@@ -0,0 +1,6 @@
+export default {
+  'title': 'README.txt',
+  'subtitle': 'Clube de Leitura de Segurança da Informação e Proteção de Dados',
+  'description': 'O "README.txt" é um Clube de Leitura idealizado e mediado por Caroline Valentim. É dedicado a temas de Segurança da Informação e Proteção de Dados, com o propósito de explorar e debater livros, artigos e demais textos dessas áreas.',
+  'button': 'Quero Participar',
+}
