f4baefed244ffad3f151d99cd6526df37f033d9a
Vitor Sanches <viisanches98@gmail.com>
2024-02-06 22:25:34 -0300

    feat: SEO

--- src/content/mediation.ts
+++ src/content/mediation.ts
@@ -10,12 +10,14 @@ interface MediationContent {
   title: string;
   description: string;
   socialMedia: SocialMedia[];
+  imageDescription: string;
 }
 
 const mediation: MediationContent = {
   name: 'Caroline Valentim',
   title: 'CIPM e CDPO/BR - IAPP | Founder da Ston Digital - Consultoria em Privacidade e Proteção de Dados | Advogada | Graduada em Segurança da Informaçãodios',
   description: 'Profissional empreendedora com dupla formação (jurídica e técnica) e vivência internacional, com experiência comprovada na captação de clientes e na assessoria a empresas de setores variados em temas de privacidade, proteção de dados e segurança da informação.',
+  imageDescription: 'Foto de Caroline Valentim. Caroline tem cabelos castanhos, olhos verdes e está vestindo um blazer listrado preto e branco. Atrás dela é possível ver a cidade de São Paulo em desfoque.',
   socialMedia: [
     { name:'linkedin', url: 'https://linkedin.com/in/carolinevalentim' },
     { name:'instagram', url: 'https://instagram.com/carolinevalentim' },
--- src/env.ts
+++ src/env.ts
@@ -10,3 +10,11 @@ export const NOTION_READINGS_DATABASE_ID = import.meta.env.NOTION_READINGS_DATAB
 export const TELEGRAM_BASE_URL = import.meta.env.TELEGRAM_BASE_URL || 'https://api.telegram.org';
 export const TELEGRAM_BOT_TOKEN = import.meta.env.TELEGRAM_BOT_TOKEN;
 export const TELEGRAM_CHAT_ID = import.meta.env.TELEGRAM_CHAT_ID;
+
+
+// Public
+export const PUBLIC_JOIN_GROUP_URL= import.meta.env.PUBLIC_JOIN_GROUP_URL || 'https://www.linkedin.com/groups/9585480/';
+export const PUBLIC_READING_BOARD_URL= import.meta.env.PUBLIC_READING_BOARD_URL || 'https://app.readmetxt.com.br';
+
+
+export const PUBLIC_CANONICAL_URL = import.meta.env.PUBLIC_CANONICAL_URL || 'https://readmetxt.com.br/';
