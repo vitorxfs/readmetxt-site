06de68c0af53fb754a70d19426e6004b12399da2
Vitor Sanches <viisanches98@gmail.com>
2024-03-06 11:50:39 -0300

    content: content review

--- src/content/howitworks.ts
+++ src/content/howitworks.ts
@@ -1,5 +1,5 @@
 export default [
-  'Escolhemos um livro por mês relacionado a segurança da informação e proteção de dados 📖',
-  'É definido um prazo para conclusão da leitura. Hora de estudar! 🤓',
-  'Realizamos um encontro online para conversarmos sobre a leitura 📆',
+  'Um livro sobre segurança da informação e/ou proteção de dados pessoais é escolhido, com uma meta de leitura máxima estabelecida em 10 páginas por dia 📖',
+  'Com o prazo final já estabelecido, chegou o momento de mergulhar nos estudos! 🤓 Criamos grupos tanto no LinkedIn quanto no WhatsApp para compartilhar nossas impressões e comentários ao longo da leitura.',
+  'Realizamos uma sessão virtual de 1h30 para discutir a leitura selecionada, dividida em três partes: comentários da mediadora, apresentações dos participantes com tempos definidos e discussão aberta ao público 📆.',
 ];
--- src/content/mediation.ts
+++ src/content/mediation.ts
@@ -13,8 +13,8 @@ interface MediationContent {
 
 const mediation: MediationContent = {
   name: 'Caroline Valentim',
-  title: 'CIPM e CDPO/BR - IAPP | Founder da Ston Digital - Consultoria em Privacidade e Proteção de Dados | Advogada | Graduada em Segurança da Informaçãodios',
-  description: 'Profissional empreendedora com dupla formação (jurídica e técnica) e vivência internacional, com experiência comprovada na captação de clientes e na assessoria a empresas de setores variados em temas de privacidade, proteção de dados e segurança da informação.',
+  title: 'CIPM e CDPO/BR - IAPP | Founder da Ston Digital - Consultoria em Privacidade e Proteção de Dados | Advogada | Graduada em Segurança da Informação',
+  description: 'Profissional empreendedora com dupla formação (jurídica e técnica) e vivência internacional, com experiência na assessoria a empresas de setores variados em temas de privacidade, proteção de dados e segurança da informação. Apaixonada por livros e café.',
   imageDescription: 'Foto de Caroline Valentim. Caroline tem cabelos castanhos, olhos verdes e está vestindo um blazer listrado preto e branco. Atrás dela é possível ver a cidade de São Paulo em desfoque.',
   socialMedia: [
     { name:'linkedin', url: 'https://linkedin.com/in/carolinevalentim' },
